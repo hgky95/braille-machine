0245804ddb0612c5462ccc5ff41369bd7312f306
EngWithData <ppaneri@algomau.ca>
2024-11-07 11:37:44 -0500

    Add files via upload

--- braille_reader/braille_reader.py
+++ braille_reader/braille_reader.py
@@ -1,6 +1,6 @@
 import cv2
 import numpy as np
-#import pytesseract
+# import pytesseract
 from gtts import gTTS
 import os
 import img_to_braille_txt
@@ -18,6 +18,7 @@ def text_to_speech(english_text):
 
     # Saving the converted audio to a file
     tts.save("../output/output.mp3")
+
 
 def braille_to_text(braille_string):
     # Translate each Braille character to English text
@@ -38,6 +39,24 @@ def image_to_speech(english_text):
     print("Audio file successfully save at ../output/output.mp3")
 
 
+# Mapping of English alphabet to Braille characters (Grade 1 Braille)
+english_to_braille = {
+    "a": "⠁", "b": "⠃", "c": "⠉", "d": "⠙", "e": "⠑",
+    "f": "⠋", "g": "⠛", "h": "⠓", "i": "⠊", "j": "⠚",
+    "k": "⠅", "l": "⠇", "m": "⠍", "n": "⠝", "o": "⠕",
+    "p": "⠏", "q": "⠟", "r": "⠗", "s": "⠎", "t": "⠞",
+    "u": "⠥", "v": "⠧", "w": "⠺", "x": "⠭", "y": "⠽", "z": "⠵",
+    " ": " "
+    # Space remains the same
+    # Add more symbols if needed
+}
+
+
+def text_to_braille(text):
+    # Convert each character to Braille using the dictionary
+    braille_text = ''.join(english_to_braille.get(char.lower(), '?') for char in text)
+    return braille_text
+
 
 # Mapping of Braille characters to English alphabet (Grade 1 Braille)
 braille_to_english = {
@@ -46,11 +65,10 @@ braille_to_english = {
     "⠅": "k", "⠇": "l", "⠍": "m", "⠝": "n", "⠕": "o",
     "⠏": "p", "⠟": "q", "⠗": "r", "⠎": "s", "⠞": "t",
     "⠥": "u", "⠧": "v", "⠺": "w", "⠭": "x", "⠽": "y", "⠵": "z",
-    "⠼": "#", # Number sign in Braille
-    "⠴": " ", # Space in Braille
+    "⠼": "#",  # Number sign in Braille
+    "⠴": " ",  # Space in Braille
     # Add more Braille symbols as needed
 }
-
 
 # Testing:
 # Specify the path to your text file to be converted into speech
@@ -81,8 +99,10 @@ print("Text has been saved to", file_path)
 english_text_from_img = image_to_braille_text_conversion(image_path)
 print("Braille image to English converted Text: " + english_text_from_img)
 
+braill_text_from_img = text_to_braille(english_text_from_img)
 
+file_path = '../output/braille_text_from_img_output.txt'
+with open(file_path, 'w', encoding='utf-8') as file:
+    file.write(braill_text_from_img)
 
-
-
-
+print("Text has been saved to", file_path)
